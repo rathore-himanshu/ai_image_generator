9422d6c5b5aacd031f28c6b70baff050ee6aec1e
rathore-himanshu <himanshu.2922t@gmail.com>
2023-03-04 09:52:00 +0530

    Added deployed web services url

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -30,7 +30,7 @@ const CreatePost = () => {
         if (form.prompt) {
             try {
                 setGeneratingImg(true);
-                const response = await fetch('http://localhost:8080/api/v1/ai', {
+                const response = await fetch('https://ai-splash.onrender.com/api/v1/ai', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
@@ -60,7 +60,7 @@ const CreatePost = () => {
             setLoading(true);
 
             try {
-                const response = await fetch('http://localhost:8080/api/v1/post', {
+                const response = await fetch('https://ai-splash.onrender.com/api/v1/post', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
